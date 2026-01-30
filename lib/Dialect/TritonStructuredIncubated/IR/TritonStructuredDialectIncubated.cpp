716311156c4e93dd9e3b1c95d73c311b2bd48dd6
CHAIYD-2025 <chaiyd_z@163.com>
2026-01-30 10:49:50 +0000

    This commit is for solve the problem of TritonStructured
    Regarding the conflict between the TritonStructured
      in triton-ascend and the TritonStructured in FLIR:
    The current build fails with the error “cannot find -lTritonStructuredIR”.
    
    Modifying CMakeLists.txt to build the original TritonStructured instead
      leads to a duplicate definition error: “mlir::tts::GetStructuredStateOp::build”.

---
+++ lib/Dialect/TritonStructuredIncubated/IR/TritonStructuredDialectIncubated.cpp
@@ -0,0 +1,24 @@
+// FLAGTREE
+#include "incubated/Dialect/TritonStructuredIncubated/IR/TritonStructuredDialectIncubated.h"
+// #include "Dialect/TritonStructured/IR/TritonStructuredDialect.h"
+
+using namespace mlir;
+using namespace mlir::tts;
+
+/// Dialect creation, the instance will be owned by the context. This is the
+/// point of registration of custom types and operations for the dialect.
+void TritonStructuredDialectIncubated::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include "incubated/Dialect/TritonStructuredIncubated/IR/TritonStructuredOpsIncubated.cpp.inc"
+      >();
+}
+
+//===----------------------------------------------------------------------===//
+// TableGen'd op method definitions
+//===----------------------------------------------------------------------===//
+
+#define GET_OP_CLASSES
+#include "incubated/Dialect/TritonStructuredIncubated/IR/TritonStructuredOpsIncubated.cpp.inc"
+
+#include "incubated/Dialect/TritonStructuredIncubated/IR/TritonStructuredDialectIncubated.cpp.inc"
